e5fa757874d53a616737f9dacefdd5c004579c47
Dominik Tabak <tabakdominik@gmail.com>
2021-08-06 15:40:50 +0200

    Developed basic functionality
    - Reading the file specified as the first argument
    - Parsing JSON into object (doesn't work with nested objects as properties)
    - Coverting the object properties into CSV text
    - Writing the CSV text into a file specified as the second argument

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,46 @@
+#! /usr/bin/env node
 'use strict';
 
+const fs = require('fs');
 const utils = require('./utils');
 const yargs = require('yargs');
 
-const usage = '\nUsage: json2csv';
+const usage = '\nUsage: json-csv-converter <json_file_path> <csv_file_path>';
 
-const options = yargs(process.argv.slice(2))
+const yargsOptions = yargs(process.argv.slice(2))
   .usage(usage)
   .help(true)
   .argv;
 
-const files = utils.parseFilePaths(yargs.argv._);
+const [jsonFilePath, csvFilePath] = utils.parseFilePaths(yargsOptions._);
 
-console.log(files);
+const jsonFile = fs.readFileSync(jsonFilePath);
+
+const arr = JSON.parse(jsonFile);
+
+let csvCode = '';
+
+Object.keys(arr[0]).forEach((key, i) => {
+  csvCode += key;
+  const isLastValue = (Object.keys(arr[0]).length - 1) === i;
+  if (isLastValue) return;
+  csvCode += ',';
+});
+
+csvCode += '\n';
+
+arr.forEach(element => {
+  const values = Object.values(element);
+  values.forEach((val, i) => {
+    csvCode += val;
+    const isLastValue = (values.length - 1) === i;
+    if (isLastValue) return;
+    csvCode += ',';
+  });
+  csvCode += '\n';
+});
+
+fs.writeFile(csvFilePath, csvCode, err => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
