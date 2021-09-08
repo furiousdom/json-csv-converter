da61f5d3637a33d3baf102f27d7fd435c23fd1b4
Dominik Tabak <tabakdominik@gmail.com>
2021-09-08 11:16:03 +0200

    Added ability to set new property values

--- bin/utils.js
+++ bin/utils.js
@@ -2,6 +2,7 @@
 
 const { EOL } = require('os');
 const { readInput } = require('../lib/fileHandler');
+const prompt = require('prompt-sync')({ sigint: true });
 
 function parseFilePaths(argv) {
   const errMsg = 'Missing arguments. Please provide both input and output paths.';
@@ -19,6 +20,10 @@ function parseOptions(argv) {
 
 module.exports = { parseFilePaths, parseOptions };
 
+function getDefaultOutputPath(inputPath) {
+  return inputPath.slice(0, inputPath.lastIndexOf('.')).concat('.csv');
+}
+
 const camelize = name => name.replace(/-./g, x => x.toUpperCase()[1]);
 
 function getOptions(argv) {
@@ -29,7 +34,8 @@ function getOptions(argv) {
     propSeparator: (argv.propSeparator || argv.s) || '/',
     delimiter: (argv.delimiter || argv.d) || ',',
     excludeProps: (argv.excludeProps || argv.e) || [],
-    renameProps: getRenamers(argv.renameProps || argv.r)
+    renameProps: getRenamers(argv.renameProps || argv.r),
+    setPropValues: getSetters(argv.setPropValues || argv.v)
   };
 }
 
@@ -40,23 +46,28 @@ function parseEOL(eolFlag) {
 }
 
 function getRenamers(props) {
+  const checker = it => it instanceof Object && 'oldPath' in it && 'newPath' in it;
+  const prompter = oldPath => `How do you want to rename "${oldPath}" property? `;
+  return getTransformers(props, checker, prompter);
+}
+
+function getSetters(props) {
+  const checker = it => it instanceof Object && 'prop' in it && 'value' in it;
+  const prompter = prop => `Enter value to set "${prop}" property to? `;
+  return getTransformers(props, checker, prompter);
+}
+
+function getTransformers(props, checker, prompter) {
   if (!props?.length) return [];
-  const renameCheck = it => it instanceof Object && 'oldPath' in it && 'newPath' in it;
-  const { renamers, propsForPrompt } = props.reduce((res, item) => {
-    res[renameCheck(item) ? 'renamers' : 'propsForPrompt'].push(item);
+  const { transformers, propsForPrompt } = props.reduce((res, item) => {
+    res[checker(item) ? 'transformers' : 'propsForPrompt'].push(item);
     return res;
-  }, { renamers: [], propsForPrompt: [] });
-  const promptedRenamers = renamePrompt(propsForPrompt);
-  return renamers.concat(promptedRenamers);
+  }, { transformers: [], propsForPrompt: [] });
+  const promptedTransformers = tranformerPrompt(propsForPrompt, prompter);
+  return transformers.concat(promptedTransformers);
 }
 
-function renamePrompt(props) {
+function tranformerPrompt(props, prompter) {
   if (!props?.length) return [];
-  const prompt = require('prompt-sync')({ sigint: true });
-  const promptText = oldPath => `How do you want to rename "${oldPath}" property? `;
-  return props.map(oldPath => ({ oldPath, newPath: prompt(promptText(oldPath)) }));
-}
-
-function getDefaultOutputPath(inputPath) {
-  return inputPath.slice(0, inputPath.lastIndexOf('.')).concat('.csv');
+  return props.map(prop => ({ prop, value: prompt(prompter(prop)) }));
 }
--- bin/yargs/flags.js
+++ bin/yargs/flags.js
@@ -8,5 +8,6 @@ module.exports = {
   propSeparator: { alias: 's', type: 'string', demandOption: false, describe: 'Enter a separator for object\'s flattened property name.' },
   excludeHeader: { alias: 'h', type: 'boolean', demandOption: false, describe: 'Don\'t incldue column names.' },
   excludeProps: { alias: 'e', type: 'array', demandOption: false, describe: 'Enter the properties you want to exclude from the final document. Please specify full path (from the top level property).' },
-  renameProps: { alias: 'r', type: 'array', demandOption: false, describe: 'Enter the properties you want renamed in the final document. If provided the program will prompt for the new paths. Please specify full path (from the top level property).' }
+  renameProps: { alias: 'r', type: 'array', demandOption: false, describe: 'Enter the properties you want renamed in the final document. If provided the program will prompt for the new paths. Please specify full path (from the top level property).' },
+  setPropValues: { alias: 'v', type: 'array', demandOption: false, describe: 'Enter the properties and values to set them to.' }
 };
